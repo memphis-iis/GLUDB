d3fceb049ac2c3278bdf65d0d465bf47776e9ae8
Craig Kelly <craig.n.kelly@gmail.com>
2015-07-13 17:28:27 -0500

    Placeholder entity

---
+++ gludb/annotations.py
@@ -0,0 +1,8 @@
+from functools import wraps
+
+# TODO: this is just a single object for package testing
+#       it will change when we actually decide on a namespace
+def DataObject(f):
+    # TODO: actual work
+    return f
+    
