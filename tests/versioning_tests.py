485fb942dc34bd94d7e2c2fb233590b5b9808a9b
Craig Kelly <craig.n.kelly@gmail.com>
2015-08-03 12:36:12 -0500

    Raw ver hist testing

--- tests/versioning_tests.py
+++ tests/versioning_tests.py
@@ -4,9 +4,18 @@
 import unittest
 import json
 
-from gludb.versioning import _isstr, record_diff, record_patch
+from gludb.versioning import (
+    _isstr,
+    record_diff,
+    record_patch,
+    append_diff_hist,
+    parse_diff_hist
+)
 
-# TODO: test append_diff_hist and parse_diff_hist
+
+def cmpfilt(o):
+    return json.loads(o) if _isstr(o) else o
+
 
 class InternalUtilTesting(unittest.TestCase):
     def setUp(self):
@@ -34,8 +43,6 @@ class RawDiffTesting(unittest.TestCase):
         diff = record_diff(old, new)
         recover = record_patch(new, diff)
 
-        def cmpfilt(o):
-            return json.loads(o) if _isstr(o) else o
         self.assertEquals(recover, cmpfilt(recover))
 
         if old == new:
@@ -56,3 +63,36 @@ class RawDiffTesting(unittest.TestCase):
         self.assertDiffable(dict(), {'a': 1, 'b': 2})
         self.assertDiffable({'a': 1, 'b': 2}, dict())
         self.assertDiffable({'a': 1, 'b': 2}, {'a': 11, 'b': 22})
+
+
+class DiffHistoryTesting(unittest.TestCase):
+    def setUp(self):
+        self.start = {'a':1, 'b':11, 'c':111}
+        self.mid1 = {'a':2, 'b':22, 'c':222}
+        self.mid2 = {'a':3, 'b':33, 'c':333}
+        self.final = {'a':4, 'b':44, 'c':444}
+        self.series = [self.start, self.mid1, self.mid2, self.final]
+
+    def tearDown(self):
+        pass
+
+    def test_empty_history(self):
+        diff_hist = []
+        obj_hist = list(parse_diff_hist(self.start, diff_hist))
+
+        self.assertEquals(1, len(obj_hist))
+        self.assertEquals(self.start, obj_hist[0][0])
+        self.assertIsNone(obj_hist[0][1])
+
+    def test_full_history(self):
+        diff_hist = []
+        last_obj = self.series[0]
+        for obj in self.series[1:]:
+            diff = record_diff(last_obj, obj)
+            diff_hist = append_diff_hist(diff, diff_hist)
+            last_obj = obj
+
+        obj_hist = list(parse_diff_hist(self.final, diff_hist))
+        self.assertEquals(4, len(obj_hist))
+        obj_list = list(reversed([ver for ver,verdate in obj_hist]))
+        self.assertEquals(self.series, obj_list)
