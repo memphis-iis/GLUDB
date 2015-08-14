b28c5106e7dd3b8eacdc47be6ca0b9800f0c710e
Craig Kelly <craig.n.kelly@gmail.com>
2015-08-14 18:16:00 -0500

    todo update [ci skip]

--- gludb/backends/__init__.py
+++ gludb/backends/__init__.py
@@ -1,13 +1,2 @@
 """glubdb.backends - implementation of actual db backends
 """
-
-# TODO: MongoDB
-
-# TODO: Other potential backends:
-#       * The most popular NoSQL engines: Riak, Redis, Couch, Cassandra
-#       * Memcache (and thus MemcacheDB)?
-#       * Lucene/Solr
-#       * Other Amazon or Google data storage? (SimpleDB, etc)
-#       * should NOT recommend them, but what about Maria/MySQL and Postgres?
-#       * File/pickle store?
-#       * Python's dbm
