2b2966005890c7bff10a4c23bf155f15c54c462b
Craig Kelly <craig.n.kelly@gmail.com>
2015-08-19 12:39:10 -0500

    build script cleanup

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ echo ""
 echo "ALSO: you HAVE checked the version in setup.py, right?"
 echo ""
 read -p "Are you sure you want to continue? " -n 1 -r
-echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
