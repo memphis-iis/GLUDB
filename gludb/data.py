3c4d7f7e43d198c8054398ad8b9601a953335205
Craig Kelly <craig.n.kelly@gmail.com>
2016-05-10 15:51:48 -0500

    TODO update and doc string format cleanup

--- gludb/data.py
+++ gludb/data.py
@@ -1,6 +1,6 @@
-"""gludb.data
+"""gludb.data - "core" functionality.
 
-The "core" functionality. If you're unsure what to use, you should look into
+If you're unsure what to use, you should look into
 gludb.simple. This module is for those needing advanced functionality or
 customization
 """
@@ -9,11 +9,12 @@ from abc import ABCMeta, abstractmethod
 
 from .config import get_mapping
 
+# pylama:ignore=E501
+
 
 # A little magic for using metaclasses with both Python 2 and 3
 def _with_metaclass(meta, *bases):
-    """Taken from future.utils, who took is from  from jinja2/_compat.py.
-    License: BSD."""
+    """Taken from future.utils, who took it from jinja2/_compat.py (BSD license)."""
     class metaclass(meta):
         __call__ = type.__call__
         __init__ = type.__init__
@@ -27,10 +28,12 @@ def _with_metaclass(meta, *bases):
 
 
 class Storable(_with_metaclass(ABCMeta)):
-    """Our abstract base class that marks subclasses as persistable and
-    storable. Note that the DBObject annotation in gludb.simple registers
-    all annotated classes as 'virtual base classes' of Storage so that you
-    can test them with isinstance(obj, Storable)"""
+    """Our abstract base class.
+
+    It marks subclasses as persistable and storable. Note that the DBObject
+    annotation in gludb.simple registers all annotated classes as 'virtual base
+    classes' of Storage so that you can test them with isinstance(obj, Storable)
+    """
 
     # This is field we use for the original version of the object (saved after
     # retrieval and before any edits occur)
@@ -39,46 +42,49 @@ class Storable(_with_metaclass(ABCMeta)):
     @classmethod
     @abstractmethod
     def get_table_name(self):
-        """Return the name of the table/collection/etc where objects should
-        be saved/loaded"""
+        """Return the name of the table/collection/etc where objects should be saved/loaded."""
         pass
 
     @abstractmethod
     def get_id(self):
-        """The instance should return the current key/ID for the instance. If
-        a 'falsey' value is return, on save one will be created and set via a
-        call to self.set_id"""
+        """The instance should return the current key/ID for the instance.
+
+        If a 'falsey' value is return, on save one will be created and set via a
+        call to self.set_id
+        """
         pass
 
     @abstractmethod
     def set_id(self, new_id):
-        """The instance should accept a new key/ID. See also get_id"""
+        """The instance should accept a new key/ID. See also get_id."""
         pass
 
     @abstractmethod
     def to_data(self):
-        """The instance should return JSON representation of it's internal
-        state. See also from_data"""
+        """The instance should return JSON representation of it's internal state. See also from_data."""
         pass
 
     @classmethod
     @abstractmethod
     def from_data(self):
-        """This classmethod returns a new instance of the subclass populated
-        from the JSON representation"""
+        """Return a new instance of the subclass populated from the JSON representation."""
         pass
 
     @classmethod
     def index_names(self):
-        """This optional method should return an iterable of index names. These
-        names should correspond to the names used in the dictionary returned by
-        the instance method `indexes` (below)"""
+        """Return an iterable of index names.
+
+        Optional method. These names should correspond to the names used in the
+        dictionary returned by the instance method `indexes` (below).
+        """
         return None
 
     def indexes(self):
-        """This optional method should return a dictionary of index name values
-        that can be used in a query. Note that this is not considered required
-        data, so a backend could ignore indexes if necessary."""
+        """Return a dictionary of index name values that can be used in a query.
+
+        Optional method. Note that this is not considered required data, so a
+        backend could ignore indexes if necessary.
+        """
         return None
 
 
@@ -121,10 +127,17 @@ def _save(self):
     setattr(self, Storable.ORIG_VER_FIELD_NAME, self.to_data())
 
 
+# TODO: we need a function ensure_package_db - it should work mainly like the
+#       package_add functionality in Backup. Once the class list is create,
+#       we would loop over every class and call ensure_table
+
+
 def DatabaseEnabled(cls):
-    """Classes annotated with DatabaseEnabled gain persistence methods. All
-    this really does is add some functions that forward to the mapped database
-    class"""
+    """Given persistence methods to classes with this annotation.
+
+    All this really does is add some functions that forward to the mapped
+    database class.
+    """
     if not issubclass(cls, Storable):
         raise ValueError(
             "%s is not a subclass of gludb.datab.Storage" % repr(cls)
@@ -140,8 +153,11 @@ def DatabaseEnabled(cls):
 
 
 def orig_version(obj):
-    """Return the original version of an object (defined as what was read from
-    the database before any user edits). If there isn't a previous version (for
-    instance, newly created objects don't have a previous version), then None
-    is returned. Mainly useful for testing"""
+    """Return the original version of an object.
+
+    Original version is defined as what was read from the database before any
+    user edits. If there isn't a previous version (for instance, newly created
+    objects don't have a previous version), then None is returned. Mainly useful
+    for testing.
+    """
     return getattr(obj, Storable.ORIG_VER_FIELD_NAME, None)
